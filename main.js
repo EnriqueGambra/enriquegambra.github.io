5036e82311e4015e31fc25c52463810714588891
enriquegambra <gambra7@gmail.com>
2024-10-21 15:31:38 -0400

    removed secret.

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// import { Octokit } from "@octokit/rest";
-//
-// const octokit = new Octokit({
-//     auth: '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',
-// });
-
-
 window.onload = () => onPageLoad()
 
 function downloadResume() {
@@ -54,34 +47,3 @@ async function buildProjectCardsFromServer() {
     document.getElementById('alien-invasion-learn-more-btn').href = projectCardData.alienInvasionData.html_url;
 
 }
-
-// async function buildProjectCards() {
-//
-//     // CPU Simulator Project
-//     const cpuSimulatorData = await octokit.request("GET /repos/enriquegambra/CPU_Simulator");
-//
-//     document.getElementById('cpu-simulator-paragraph').innerText = cpuSimulatorData.data.description.substring(0, 100) + '...';
-//
-//     document.getElementById('cpu-simulator-learn-more-btn').href = cpuSimulatorData.data.html_url;
-//
-//     // Amazon Alexa Data
-//     const amazonAlexaData = await octokit.request("GET /repos/enriquegambra/Amazon-Alexa-Skill");
-//
-//     document.getElementById('amazon-alexa-paragraph').innerText = amazonAlexaData.data.description.substring(0, 100) + '...';
-//
-//     document.getElementById('amazon-alexa-skill-learn-more-btn').href = amazonAlexaData.data.html_url;
-//
-//     // Compare-NBA-Players-Stats Data
-//     const nbaPlayerStats = await octokit.request("GET /repos/enriquegambra/Compare-NBA-Players-Stats");
-//
-//     document.getElementById('nba-player-stats-paragraph').innerText = nbaPlayerStats.data.description.substring(0, 100) + '...';
-//
-//     document.getElementById('nba-player-stats-learn-more-btn').href = nbaPlayerStats.data.html_url;
-//
-//     // Alien-Invasion Data
-//     const alienInvasionData = await octokit.request("GET /repos/enriquegambra/Alien-Invasion");
-//
-//     document.getElementById('alien-invasion-paragraph').innerText = alienInvasionData.data.description.substring(0, 100) + '...';
-//
-//     document.getElementById('alien-invasion-learn-more-btn').href = alienInvasionData.data.html_url;
-// }
